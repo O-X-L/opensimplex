5f16a06d719d273a5dea5ad7ab6e3187762e5c59
Rath Pascal <rath@oxl.at>
2025-01-13 21:46:35 +0100

    test fix

--- examples/minimal.py
+++ examples/minimal.py
@@ -4,6 +4,7 @@ from sys import path as sys_path
 BASE_PATH = Path(__file__).parent.parent.resolve()
 sys_path.append(str(BASE_PATH))
 
+# pylint: disable=C0413
 from opensimplex import OpenSimplex
 
 print('2D Noise Sample:', OpenSimplex().get_2d(20, 40))
